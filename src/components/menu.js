e01371eea12281c0f3ae8e91f9e375545b252952
Gbaja <gbajaf@yahoo.co.uk>
2018-06-29 14:07:23 +0100

    Fixed mistake from previous deploy

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,48 +1,22 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from "react";
+import Link from "gatsby-link"
 
-export default class Contact extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out: <input name="bot-field" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-      </div>
-    )
-  }
-}
+const Menu = () => (
+  <div className="nav__container">
+    <ul className="nav__ul">
+      <li className="nav__list">
+        <Link to="/about" className="nav__link">About</Link>
+      </li>
+        <li className="nav__list">
+            <Link to="/blog" className="nav__link">Blog</Link>
+      </li>
+      <li className="nav__list">
+        <Link to="/contact" className="nav__link">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </div>
+)
+
+    export default Menu;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,22 +2,28 @@ import React from 'react'
 import './pages.css'
 
 const ContactPage = () => (
-    <div>
-        <form name="contact" method="POST" data-netlify="true">
-            <p>
-                <label>Your Name: <input type="text" name="name" /></label>
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-                <label>Message: <textarea name="message"></textarea></label>
-            </p>
-            <p>
-                <button type="submit">Send</button>
-            </p>
-        </form>
-    </div>
-);
+  <div>
+    <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+      <p>
+        <label>
+          Your Name: <input type="text" name="name" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Your Email: <input type="email" name="email" />
+        </label>
+      </p>
+      <p>
+        <label>
+          Message: <textarea name="message" />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
+)
 
 export default ContactPage
