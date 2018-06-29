834a6e33275f0afd35f6ed20f45e389425edefae
Gbaja <gbajaf@yahoo.co.uk>
2018-06-29 13:35:44 +0100

    Fixed the form page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,30 +1,23 @@
 import React from 'react'
 import './pages.css'
 
-export default class Contact extends React.Component{
-  render(){
-  return (<div>
-    <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-      <p>
-        <label>
-          Your Name: <input type="text" name="name" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your Email: <input type="email" name="email" />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message: <textarea name="message" />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
-    </form>
-  </div>
-)
-  }
-}
+const ContactPage = () => (
+    <div>
+        <form name="contact" method="POST" data-netlify="true">
+            <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
+        </form>
+    </div>
+);
+
+export default ContactPage
