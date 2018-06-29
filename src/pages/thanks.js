156e2b5ccb9b35bd21686f40f9c4c32caba93d95
Gbaja <gbajaf@yahoo.co.uk>
2018-06-29 17:45:39 +0100

    Changed thanks file a bit

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Link from 'gatsby-link'
+import "./pages.css"
 
-const Thanks = ()=> <div>
-        <h3> Thank you for contact Sine Nomine </h3>
-        <p> I aim to respond back with 7 days</p>
-        <Link to="/">Back to Sine Nomine </Link>
+const Thanks = ()=> <div className="thanks__container">
+        <h3 className="thanks__title"> Thank you for contacting Sine Nomine. </h3>
+        <p> I aim to respond back within 7 days.</p>
+        <p>Keep learning!</p>
     </div>
 
 export default Thanks
